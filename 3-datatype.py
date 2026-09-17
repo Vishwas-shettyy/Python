1ee78d003aab8cf5fee8993b6506ae17752b0812
Vishwas27 <vikarna2426@gmail.com>
2026-09-17 23:08:09 +0530

    Python data types understanding basic program

---
+++ 3-datatype.py
@@ -0,0 +1,9 @@
+name = "vishwas"  # string
+age = 20  # integer
+weight = 69.5  # float
+is_student = False  # bool
+
+print(type(is_student))  # it prints the type of the datatype
+print(
+    name, age, weight, is_student, sep="\n"
+)  # sep="\n" means separate each value with a new line.
