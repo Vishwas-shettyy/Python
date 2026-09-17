10d74799d144572c1b404c4edac97c933603e135
Vishwas27 <vikarna2426@gmail.com>
2026-09-17 23:07:35 +0530

    Python variables understanding basic program

---
+++ 2-variable.py
@@ -0,0 +1,21 @@
+a = 10
+b = 20
+
+a, b, c = 10, 20, 30
+
+a = b = c = 10
+#those are the different ways to Creating variable
+
+
+name = "Vishwas"
+is_switch_on = "true"
+
+print(a)
+print(a + b)
+
+# Variable name
+name="visu"
+Name="VISU" #case sensitivity
+
+print(Name) #this return the value stored in variable Name
+print("Name") #this just prints the Name in screen
