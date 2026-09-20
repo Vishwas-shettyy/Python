e59bf6b182e36b6c16de428500ebdc179b111eb4
Vishwas27 <vikarna2426@gmail.com>
2026-09-20 21:26:20 +0530

    Basic user input question

---
+++ 8-q.py
@@ -0,0 +1,6 @@
+first = int(input("Enter the 1st number:"))
+second = int(input("Enter the 2st number:"))
+
+sum = first + second
+# or print("sum:", first + second )
+print("Sum of 2numebrs:", sum)
