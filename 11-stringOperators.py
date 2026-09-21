86bcc2e4bed84fe15a122a796361bd69953f08ac
Vishwas27 <vikarna2426@gmail.com>
2026-09-21 21:38:10 +0530

    types of string operation

---
+++ 11-stringOperators.py
@@ -0,0 +1,27 @@
+# ----- STRING OPERATIONS -----
+text = "Python Programming"
+
+
+# ----- LENGTH -----
+print("Length:", len(text))
+
+
+# ----- INDEXING -----
+print("First character:", text[0])
+print("Second character:", text[1])
+print("Last character:", text[-1])
+
+# Checking type
+name = "Vishwas"
+print("Type:", type(name))
+print("\n")
+
+# Concatenation
+first_name = "Vishwas"
+last_name = "Shetty"
+print("Full name:", first_name + " " + last_name)
+print("\n")
+
+# String repetition
+word = "Python "
+print("Repeated:", word * 3)
