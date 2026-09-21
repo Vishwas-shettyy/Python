c860cd4715790cf528bfc9c33c8e71c65ccad23c
Vishwas27 <vikarna2426@gmail.com>
2026-09-21 21:36:58 +0530

    Type of string insitialiaztion

---
+++ 10-string.py
@@ -0,0 +1,56 @@
+# Strings
+
+# Single quotes
+name = 'Vishwas'
+print("Single quotes:", name)
+print("\n")
+
+
+# Double quotes
+name = "Vishwas"
+print("Double quotes:", name)
+print("\n")
+
+# Triple double quotes
+message = """Hello Vishwas"""
+print("Triple double quotes:", message)
+print("\n")
+
+# Triple single quotes
+message = '''Hello Vishwas'''
+print("Triple single quotes:", message)
+print("\n")
+
+# Multi-line string
+message = """Hello
+My name is Vishwas
+I am learning Python"""
+print("Multi-line string:")
+print(message)
+print("\n")
+
+# Single quote inside double quotes
+message = "I'm learning Python"
+print(message)
+print("\n")
+
+# Double quote inside single quotes
+message = 'He said "Hello"'
+print(message)
+print("\n")
+
+# Escape character
+message = "He said \"Hello\""
+print(message)
+print("\n")
+
+# Empty string
+name = ""
+print("Empty string:", name)
+print("\n")
+
+# String with numbers
+roll_no = "101"
+print("Roll number:", roll_no)
+print("\n")
+
