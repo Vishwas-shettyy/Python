9f70b21f1ba8a9415f9b501dc91037fa57a37605
Vishwas27 <vikarna2426@gmail.com>
2026-09-21 21:47:46 +0530

    Slicing: Unique topic in python

---
+++ 12-slicing.py
@@ -0,0 +1,32 @@
+# ----- SLICING -----
+
+text = "Python Programming"
+
+print("Original:", text)
+
+print("text[0:6]:", text[0:6])
+# Python
+
+print("text[6:7]:", text[6:7])
+# Space
+
+print("text[7:18]:", text[7:18])
+# Programming
+
+print("text[:6]:", text[:6])
+# Python
+
+print("text[7:]:", text[7:])
+# Programming
+
+print("text[:]:", text[:])
+# Python Programming
+
+print("text[::2]:", text[::2])
+# Every 2nd character
+
+print("text[::3]:", text[::3])
+# Every 3rd character
+
+print("text[::-1]:", text[::-1])
+# Reverse the string
